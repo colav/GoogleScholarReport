273ab000d326c294253691bba9c3e1c0a41b6e12
RWC <39197176+readingwritingcode@users.noreply.github.com>
2021-06-03 17:49:52 -0500

    Create main.py
    main code v 0.5

---
+++ google-scholar-report/main.py
@@ -0,0 +1,308 @@
+#/usr/bin/python3
+import re
+import os
+import sys
+import time
+import argparse
+
+import lxml
+import helium
+
+import unidecode
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import convert_to_unicode
+
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+import GoogleScholarProfile as gsp
+
+import pandas as pd
+
+def command_line_parser():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("url", help="public url for google scholar profile", type=str)
+
+    parser.add_argument("--email", help="email for google scholar account",type=str)
+
+    parser.add_argument("--password", help="password for google scholar account",type=str)
+
+    parser.add_argument("--admin", help="adming mode True or False",type=bool)
+
+    args = parser.parse_args()
+
+    return args
+
+def read_bibtex(str_bib_tex):
+    '''
+    Read bibtext database and return a dictionary
+    '''
+    parser = BibTexParser(common_strings=True)
+    
+    parser.ignore_nonstandard_types = False
+    
+    parser.homogenize_fields = True
+    
+    parser.customization = convert_to_unicode
+
+    bib_data = bibtexparser.loads(str_bib_tex, parser=parser)
+
+    return bib_data
+
+def unidecode_character(match):
+    """
+    This function takes a re 'match object' and performs
+    The appropriate substitutions
+    """
+
+    group = match.group(1)
+    return 'í'+unidecode.unidecode(group)
+
+def scraping_gs(url):
+    # Scraping Google Scholar
+ # Scraping Google Scholar
+    #url='https://scholar.google.com/citations?hl=en&user=5Fh1OLwAAAAJ'
+
+    url=re.sub(r'hl=[a-zA-Z][a-zA-Z]','hl=en', url)
+
+    #    '''
+    #    Go to google scholar profile and make scroll to end and catch two html tables: scientific production and index citations. 
+    #  '''
+
+    browser=helium.start_firefox(url,headless=True)
+    clicks=0
+
+        # Scroll to end
+    while helium.Button.is_enabled(helium.Button('SHOW MORE')):
+
+        helium.scroll_down(200000)
+        print('click',':',clicks)
+        helium.click(helium.Button('SHOW MORE'))
+        helium.scroll_down(100000)
+        time.sleep(1.5)
+        clicks+=1
+    
+
+    # get scraping data and metadata
+    rec=gsp.GoogleScholarProfile(browser.page_source)
+
+    # handle scraping data -> not login 
+    dfs=pd.DataFrame(data=rec)
+
+    browser.close()
+    
+    return dfs
+
+def export_gs(email,password):
+    
+    '''This function automatice log-in and export article records from Google scholar profile'''
+    
+    sleep = 1.5
+    url_to_gs='https://scholar.google.com/citations?hl=es&login' 
+
+    browser=helium.start_firefox(url_to_gs,headless=True)
+
+    # Login
+    helium.write(email,into='Correo electrónico o teléfono')
+
+    time.sleep(sleep)
+    helium.click(helium.Button('Siguiente'))
+
+    time.sleep(sleep)
+    helium.write(password,into='Introduce tu contraseña')
+
+    time.sleep(sleep)
+    helium.click(helium.Button('Siguiente'))
+
+    #wait until load page
+    helium.wait_until(helium.Text('TÍTULO').exists)
+
+    # Select articles
+    time.sleep(3)
+    browser.find_element_by_id('gsc_x_all').click()
+
+    time.sleep(sleep)
+    helium.click('EXPORTAR')
+
+    time.sleep(sleep)
+    helium.click('BibTeX')
+
+    time.sleep(sleep)
+    helium.click('Exportar todos mis artículos')
+
+    time.sleep(sleep)
+    # Button EXPORTAR
+    browser.find_element_by_id('gsc_md_exa_export').click()
+
+    time.sleep(sleep)
+
+    # Handle response bibtext export-> high cuality metadata
+    time.sleep(sleep)
+    bib_data = read_bibtex(browser.find_element_by_tag_name('html').text)
+
+    dfe=pd.DataFrame(bib_data.entries)
+
+    dfe['ID']=dfe['ID'].apply(unidecode.unidecode)
+
+    # Handle raw bib text data
+    bib_raw=browser.find_element_by_tag_name('html').text
+
+    # Generate bib_list 
+    bib_list = [re.sub(r'^(\w)', r'@\1',l,re.UNICODE) for l in re.split(r'\n@',re.sub(r'^@',r'\n@',bib_raw)) if l]
+
+    # Generate unicode bibtext data ids
+    bib_ids = [l.split('{')[1].split(',')[0] for l in bib_list]
+    bib_ids = [unidecode.unidecode(l) for l in bib_ids]    #
+
+    # Build dict with ids and bibtex data
+    bibtex_dict = dict(zip(bib_ids,bib_list))
+
+    # Combine response bibtex and raw bibtext 
+    dfe['bibtex']=dfe['ID'].apply(lambda s: bibtex_dict.get(s))
+
+    # Validate results
+    assert dfe['bibtex'].shape[0] == dfe['bibtex'].dropna().shape[0]
+    # dfe[dfe['bibtex'].isna()]['ID']
+
+    dfe['author'].apply(lambda s: re.sub(r'\\(.)',unidecode_character,s,flags=re.I))
+    
+    return dfe
+
+def merge(dfs,dfe):
+    
+    '''Merge scraping and export google scholar profile data'''
+    
+    # preparing scraping dataframe                                           
+    dfs['TitleU']=dfs['src_title'].apply(lambda s: unidecode.unidecode(s)) 
+
+    dfs=dfs.drop_duplicates(subset=['TitleU']).reset_index(drop=True)
+
+    dfs.reset_index(drop=True)
+
+    # preparing dataframe exported gs
+    dfe['TitleU'] = dfe['title'].apply(lambda s: unidecode.unidecode(s)
+                               ).apply(lambda s: s.replace('$','')
+                               ).apply(lambda s: s.replace('--',''))
+
+    dfe = dfe.drop_duplicates(subset=['TitleU']).reset_index(drop=True)
+
+    dfe=dfe.reset_index(drop=True)
+
+    # Merge 
+    gs=dfs.merge(dfe,on='TitleU',how='left')
+        # gs=gs.drop_duplicates('TitleU').reset_index(drop=True)
+    gs=gs.reset_index(drop=True)
+    
+    GS = gs
+    
+    # fixed bad results in title column
+    gsn = gs[~gs['title'].isna()].reset_index(drop=True) # not empty
+
+    gsy = gs[gs['title'].isna()].reset_index(drop=True) # yes, empty
+
+    gsy = gsy.dropna(axis=1)
+    
+    if gsy.shape[0] > 0:
+    
+        g=pd.DataFrame()
+
+        for i in gsy.index:
+
+            d = gsy.loc[[i]].to_dict(orient='records')[0]
+
+            rs = process.extractOne(d['TitleU'],dfe['TitleU'],scorer=fuzz.partial_ratio)
+
+            if rs[1] > 90:
+
+                idx = rs[2]
+
+                d.update(dfe.loc[[idx]].to_dict(orient='records')[0])
+
+                g = g.append(d,ignore_index=True)
+                
+        GS =pd.concat([gsn,g])
+        
+    return GS
+
+def main(url, email = '', password = '',admin = ''):
+    
+    url = sys.argv[1]
+    
+    if email != '' and password !='':
+        
+        dfs = scraping_gs(url)
+
+        print('dfs.shape:',dfs.shape)
+        
+        dfe = export_gs(email, password)
+
+        print('dfe.shape:', dfe.shape)
+        
+        gsr = merge(dfs,dfe)
+
+        print('gsr.shape',gsr.shape)
+
+        
+        if admin != '':
+            
+            # store df admin
+
+            gsr.to_excel('google_scholar_report_admin.xlsx')
+
+            return 'google_scholar_report_admin.xlsx stored!'
+        
+        else:
+            # store df user authenticate
+
+            gsr=gsr.loc[:, gsr.columns != 'bibtex']
+
+            gsr.to_excel('google_scholar_report_user.xlsx')
+
+            return 'store df user authenticate'
+    
+    else:
+        
+        dfs = scraping_gs(url)
+        
+        dfs.rename(columns={    'src_title':'title',
+                                'src_author':'author',
+                                'src_journal':'journal',
+                                'src_volume':'volume', 
+                                'src_number':'number',
+                                'src_pages':'pages',
+                                'src_year':'year'
+                           }, 
+                   inplace=True)
+
+        # store df generic
+        dfs.to_excel('google_scholar_report_generic.xlsx')
+        
+        return 'generic report stored' 
+
+if __name__ == "__main__":
+
+    args = command_line_parser()
+
+    url = args.url
+
+    if args.email and args.password:
+
+        email = args.email
+
+        password = args.password
+
+        if args.admin:
+
+            admin = args.admin
+
+            main(url, mail, password, admin)
+        else:
+
+            main(url, email, password)
+    
+    else:
+
+        main(url)
