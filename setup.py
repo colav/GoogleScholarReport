c5dd39955c0eeccfae00f9ae5a07b3e2af555ec3
RWC <39197176+readingwritingcode@users.noreply.github.com>
2021-06-24 09:45:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -98,7 +98,29 @@ def main():
 				Unidecode==1.2.0,
 				urllib3==1.26.3
 
-        				]
+        		],
+	classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific communication :: Visibility',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        ]
     )
 
 
